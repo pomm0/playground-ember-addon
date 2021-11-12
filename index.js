81a6213a519302f84639df680c48c658a8c81bec
Markus Gruber <markus.gruber@husqvarnagroup.com>
2021-11-12 15:01:06 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
+const Funnel = require('broccoli-funnel');
+const Merge = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
+
+  treeForStyles(tree) {
+    const myCustomStyles = new Funnel('./lib', {
+      destDir: 'app/styles/my-custom-styles',
+    });
+
+    return new Merge([myCustomStyles, tree].filter(Boolean));
+  },
 };
